9f970e73d6e60496094fc8acfd606c37abcc64cb
Marco Tessitore <marcotexcode@gmail.com>
2021-07-23 14:29:43 +0200

    prima e seconda parte

--- script.php
+++ script.php
@@ -1,6 +1,10 @@
 <?php
 
+    //Creare una variabile con un paragrafo di testo a vostra scelta
+    $text = 'Ciao il mio nome e Paolo e ho 20 anni';
 
+    //Stampare a schermo il paragrafo e la sua lunghezza.
+    $lunghezzaText = strlen($text);
 
 ?>
 
@@ -15,7 +19,20 @@
 </head>
 <body>
 
-    <p></p>
+    <!-- Stampare a schermo  la sua lunghezza. -->
+    <h2>lunghezza: <?php echo $lunghezzaText ?></h2>
+
+    <!-- Stampare a schermo il paragrafo. -->
+    <p><?php echo $text ?></p>
+
 
 </body>
 </html>
+
+
+
+
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta. 
+Stampare a schermo il paragrafo e la sua lunghezza. 
+Una parola da censurare viene passata dall'utente tramite parametro GET. 
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
